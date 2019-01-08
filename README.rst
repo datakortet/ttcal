792bafe4384cc67a62cf04dbfe34fc8dce75abe8
Bjorn <bp@datakortet.no>
2019-01-08 17:01:28 +0100

    changes to make pypi happy

--- README.rst
+++ README.rst
@@ -3,11 +3,11 @@
     :target: https://travis-ci.org/datakortet/ttcal
 
 .. image:: https://coveralls.io/repos/github/datakortet/ttcal/badge.svg?branch=master
-    :target: https://coveralls.io/github/datakortet/ttcal?branch=master
+    :target: https://coveralls.io/github/datakortet/ttcal
 
 
-ttcal -- calendar calculations
-==================================================
+ttcal - calendar calculations
+=============================================================
 
 
 Installing from PyPI
@@ -20,12 +20,11 @@ This is what you want if you just want to use ttcal:
 
 As a source package
 -------------------
+
 This is what you want if you are developing ttcal or want 
 to make local changes to the source code.
 
    pip install -e <path>
-
-
 
 
 See docs/ folder for documentation.
--- setup.py
+++ setup.py
@@ -12,15 +12,19 @@ Intended Audience :: Developers
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.4
 Topic :: Software Development :: Libraries
 """
 
-import sys
+import sys, os
 import setuptools
 from distutils.core import setup, Command
 from setuptools.command.test import test as TestCommand
 
 version = '1.0.1'
+
+DIRNAME = os.path.dirname(__file__)
+description = open(os.path.join(DIRNAME, 'README.rst'), 'rb').read()
 
 
 class PyTest(TestCommand):
@@ -45,11 +49,14 @@ class PyTest(TestCommand):
 setup(
     name='ttcal',
     version=version,
+    url='https://github.com/datakortet/ttcal',
+    author='Bjorn Pettersen',
+    author_email='bp@datakortet.no',
     requires=[],
     install_requires=[],
-    description=__doc__.strip(),
+    # description=__doc__.strip(),
+    long_description=description,
     classifiers=[line for line in classifiers.split('\n') if line],
-    long_description=open('README.rst').read(),
     cmdclass={'test': PyTest},
     packages=['ttcal'],
     zip_safe=False,
